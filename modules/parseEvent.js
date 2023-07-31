f216636d6bfa4b2fbe13d9d50a6bad3a036c4360
maximegenand <m.genand@gmx.fr>
2023-08-01 00:31:50 +0200

    add post route for event

---
+++ modules/parseEvent.js
@@ -0,0 +1,45 @@
+
+// Fonction qui filtre les infos à renvoyer au Front en supprimant les ids et remplacant les user_id par des tokens
+const parseEvent = eventBrut => {
+  const event = eventBrut.toJSON();
+
+  // On récupère seulement les infos qu'on veut renvoyer
+  const { tokenEvent, name, date, timeStart, timeEnd, place, description, seats } = event;
+
+  const user = { tokenUser: event.user.tokenUser, username: event.user.username };
+
+  const { tokenTrip } = event.trip;
+
+  const participants = event.participants.map( obj => {
+    const { tokenUser, username, picture } = obj;
+    return { tokenUser, username, picture };
+  });
+  console.log(participants);
+
+  const infos = event.infos.map( obj => {
+    return {
+      tokenInfo: obj.tokenInfo,
+      user: { tokenUser: obj.user.tokenUser, username: obj.user.username, picture: obj.user.picture },
+      name: obj.name,
+      type: obj.type,
+      uri: obj.uri,
+    };
+  });
+
+  return {
+    tokenEvent,
+    tokenTrip,
+    name,
+    date,
+    timeStart,
+    timeEnd,
+    place,
+    description,
+    seats,
+    user,
+    participants,
+    infos,
+  }
+}
+
+module.exports = { parseEvent };
--- routes/events.js
+++ routes/events.js
@@ -1,23 +1,93 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const Trip = require("../models/trips");
 const User = require("../models/users");
 const Event = require("../models/events");
 const uid2 = require("uid2");
 
-//pas fini
-router.post("/events", async (req, res) => {
-  if (!checkBody(req.body, ["name", "description", "place", "timeStart", "timeEnd", "seats", "travelId"])) {
-    res.json({ result: false, error: "Missing or empty fields" });
-    return;
-  }
-  const {name, description, place, timeStart, timeEnd, seats, travelId, token} = req.body;
+// Import des fonctions
+const { checkBody } = require('../modules/checkBody');
+const { checkTokenTrip } = require('../modules/checkTrip');
+const { checkTokenSession, checkTokenUser } = require('../modules/checkUser');
+const { parseEvent } = require("../modules/parseEvent");
 
+
+
+// Route POST pour créer un événement
+router.post("/", async (req, res) => {
+  // On vérifie si les infos obligatoires sont bien renseignées
+  if (!checkBody(req.body, ["token", "event"])) {
+    return res.status(404).json({ result: false, error: "Missing or empty fields" });
+  }
+
+  // On récupère les infos du req.body
+  const { token, event } = req.body;
+
+  // On vérifie si l'utilisateur existe, et si oui on renvoie ses infos
+  const user = await checkTokenSession(token);
+  if(!user) {
+    return res.status(404).json({ result: false, error: "User not found" });
+  }
+
+  // On recherche un trip suivant le tokenTrip
+  const findTrip = await Trip.findOne({ tokenTrip: event.tokenTrip });
+
+  // Si on trouve pas le trip, on retourne une erreur
+  if (!findTrip) {
+    return res.status(404).json({ result: false, error: "Trip not found" });
+  }
+
+  // On vérifie si l'utilisateur à le droit d'accéder au Trip - et donc si le trip existe
+  if(!user.trips.includes(findTrip._id)) {
+    return res.status(404).json({ result: false, error: "Not allowed" });
+  }
+
+  // On transforme les infos supplémentaires pour pouvoir l'enregistrer
+  const infos = await Promise.all(event.infos.map(async obj => {
+    return {
+      tokenInfo: uid2(32),
+      user: await checkTokenUser(obj.tokenUser),
+      name: obj.name,
+      type: obj.type,
+      uri: obj.uri,
+    }
+  }));
+
+  // On créé un nouveau Event
   const newEvent = new Event({
     tokenEvent: uid2(32),
-    user : token,
+    trip: findTrip._id,
+    user: user._id,
+    name: event.name,
+    date: event.date,
+    timeStart: event.timeStart,
+    timeEnd: event.timeEnd,
+    place: event.place,
+    seats: event.seats,
+    description: event.description,
+    participants: [user._id],
+    infos,
+  });
 
-  })
+  try {
+    // On enregistre l'Event
+    await newEvent.save();
+
+    // On populate les infos de l'Event
+    await newEvent.populate([
+      { path: "user" },
+      { path: "trip" },
+      { path: "participants" },
+      { path: "infos.user" },
+    ]);
+
+    // On filtre les infos que l'on veut renvoyer en front
+    const eventRes = parseEvent(newEvent);
+    res.json({ result: true, event: eventRes });
+  } catch (error) {
+    console.error("Erreur lors de l'enregistrement de l'Event :", error);
+    res.status(404).json({ result: false, error: "Erreur lors de l'enregistrement de l'Event" });
+  }
 });
 
 module.exports = router;
