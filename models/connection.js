743728b0b2f9470c3c777d86dfd3daf90d85d47b
maximegenand <m.genand@gmx.fr>
2023-08-02 12:19:26 +0200

    try fix deployment

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true)
 
 const connectionString = process.env.CONNECTION_STRING;
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+const options = { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 10000 }
+
+mongoose.connect(connectionString, options)
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
